14c10fcada9eabe6f17c0478ff24b19c2d1a9378
prihantoro-corpus <prihantoro@live.undip.ac.id>
2025-12-09 19:06:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2628,7 +2628,7 @@ if st.session_state['view'] == 'concordance' and st.session_state.get('analyze_b
             text-align: left;
         }}
         .breakdown-table td {{
-            background-color: #1F1F1F; /* User's Row Background */
+            background-color: #444444; /* User's Row Background */
             color: #FAFAFA;          /* User's Text Color */
             padding: 8px;
             border-bottom: 1px solid #333;
@@ -3271,3 +3271,4 @@ if st.session_state['view'] == 'collocation' and st.session_state.get('analyze_b
 
 
 st.caption("Tip: This app handles pre-tagged, raw, and now **Excel-based parallel corpora**.")
+
