c5fb597d357177374cf9da26d9237f050d014e9d
prihantoro-corpus <prihantoro@live.undip.ac.id>
2025-12-11 18:28:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1829,7 +1829,7 @@ def generate_collocation_results(df_corpus, raw_target_input, coll_window, mi_mi
 # ---------------------------
 # UI: header
 # ---------------------------
-st.title("CORTEX - Corpus Texts Explorer v17.50 (Parallel Ready)")
+st.title("CORTEX - Corpus Texts Explorer by PRIHANTORO")
 st.caption("Upload vertical corpus (**token POS lemma**) or **raw horizontal text**, or **Parallel Corpus (Excel/XML)**.")
 
 # ---------------------------
@@ -3325,4 +3325,5 @@ if st.session_state['view'] == 'collocation' and st.session_state.get('analyze_b
 
 
 st.caption("Tip: This app handles pre-tagged, raw, and now **Excel-based parallel corpora**.")
+
 
