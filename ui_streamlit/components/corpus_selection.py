34a8198ea6aaef0d3534e8cdc9e0ae4a4929d67c
Prihantoro <prihantoro@live.undip.ac.id>
2026-09-22 23:02:47 +0700

    Restore Built-in Corpora button and update gitignore

--- ui_streamlit/components/corpus_selection.py
+++ ui_streamlit/components/corpus_selection.py
@@ -68,7 +68,7 @@ def render_corpus_selection_main():
                 st.session_state['sidebar_source_selectbox'] = "Upload Files"
             source_type = st.radio(
                 "Source", 
-                ["Upload Files", "Online Corpus"],
+                ["Upload Files", "Built-in Corpora", "Online Corpus"],
                 key="sidebar_source_selectbox",
                 horizontal=True
             )
@@ -77,3 +77,6 @@ def render_corpus_selection_main():
         if source_type == "Online Corpus":
             online_mode = st.radio("Builder Mode", ["YouTube", "Mastodon", "BlueSky", "Link Collection", "Keyword Search", "Detik.com"], horizontal=True, key="main_online_builder_mode")
             set_state('online_builder_mode', online_mode)
+        elif source_type == "Built-in Corpora":
+            from ui_streamlit.views.overview_view import render_built_in_corpora_selection_ui
+            render_built_in_corpora_selection_ui()
